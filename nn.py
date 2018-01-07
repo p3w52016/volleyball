8a00558a73b4e527457863143895efd83418c0e8
p3w52016 <p3w52016@gmail.com>
2018-01-07 21:01:39 +0800

    fit_generator to nn.py

--- nn.py
+++ nn.py
@@ -1,19 +1,23 @@
 import h5py
 import numpy as np
 import pickle as pk
+from keras import regularizers
 from keras.utils import to_categorical
 from keras.models import Sequential, Model
 from keras.layers import Flatten,  Reshape, TimeDistributed, Dense, Conv2D, LSTM, BatchNormalization, Dropout,\
                     Input, concatenate
-folder = 'conf.4_20P_pkl'
+from fit_generator import DataGenerator
+
+params={
+    "x": 10,
+    "y": 20,
+    "z": 36,
+    "batch_size": 128
+}
+
+folder = 'conf.3_20P_pkl'
 
 def nn_model():
-    #coordinates
-    coord_in = Input(shape=(10, 20, 36), name='coord_in')
-    coord = TimeDistributed(LSTM(1000, input_shape=(20,36), dropout=0.5), input_shape=(10,20,36))(coord_in)#out: 3000, 20, 36
-    coord = Dense(512, activation='relu')(coord)
-    coord = Dropout(0.5)(coord)
-
     #pool
     pool_in = Input(shape=(10, 74**2), name='pool_in')
     pool = Dense(2048, activation='relu')(pool_in)
@@ -30,22 +34,30 @@ def nn_model():
     conv2 = Dropout(0.5)(conv2)
 
     #dense
-    dense = Input(shape=(10, 2048))
+    dense = Input(shape=(10, 20, 2048))
+
+    #coordinates
+    coord_in = Input(shape=(10, 20, 36), name='coord_in')
+    coord = concatenate([coord_in, dense], axis=3)
+    coord = TimeDistributed(LSTM(100, input_shape=(20,36), dropout=0.5,\
+    kernel_regularizer=regularizers.l2(0.01)), input_shape=(10,20,2084))(coord_in)#out: 3000, 20, 36
+    coord = Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.01))(coord)
+    coord = Dropout(0.5)(coord)
 
     #merge_input
-    model = concatenate([dense, coord])
-
-    model = LSTM(3000, dropout=0.5)(model)
-    model = Dense(512, activation='relu')(model)
+    model = LSTM(100, dropout=0.5, kernel_regularizer=regularizers.l2(0.01))(coord)
+    model = Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.01))(model)
     model = BatchNormalization()(model)
     model = Dropout(0.5)(model)
+    '''
     model = Dense(256, activation='relu')(model)
     model = BatchNormalization()(model)
     model = Dropout(0.5)(model)
-    model = Dense(128, activation='relu')(model)
+    '''
+    model = Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.01))(model)
     model = BatchNormalization()(model)
     model = Dropout(0.5)(model)
-    model = Dense(64, activation='relu')(model)
+    model = Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.01))(model)
     model = BatchNormalization()(model)
     model = Dropout(0.5)(model)
 
@@ -55,33 +67,46 @@ def nn_model():
 
     return Model(inputs=[coord_in, dense], outputs=output)
 
-def train(batch_size=512, epochs=1000):
+def train(batch_size=params["batch_size"], epochs=500):
     #h5f = h5py.File('/free2/p3w52016/volleyball_dataset/pkl/'+folder+'/volleyball_x.h5','r')
     #x = h5f['data'][:]
-    h5f = h5py.File('/free2/p3w52016/volleyball_dataset/pkl/'+folder+'/volleyball_y.h5','r')
-    y = h5f['label'][:]
-    print(y[:5], y.shape)
-    exit()
-    y = to_categorical(y, num_classes=8)
+    #h5f = h5py.File('/free2/p3w52016/volleyball_dataset/pkl/'+folder+'/volleyball_y.h5','r')
+    #y = h5f['label'][:]
+    #print(y[:5], y.shape)
+    split=0.2
+    ID_list = np.arange(4830)
+    np.random.shuffle(ID_list)
+    ID_tr = ID_list[:int(len(ID_list)*(1-split))]
+    ID_val = ID_list[int(len(ID_list)*(1-split)):]
+
+    tr_generator = DataGenerator(**params).generate(ID_list=ID_tr, mode='tr')
+    val_generator = DataGenerator(**params).generate(ID_list=ID_val, mode='val')
     print('coords loaded')
 
     #pool = load_feature('pool', 74**2)
-    print('pool loaded')
+    #print('pool loaded')
     #conv1 = load_feature('conv1', 74**2)
-    print('cn1 loaded')
+    #print('cn1 loaded')
     #conv2 = load_feature('conv1', 74**2)
-    print('cn2 loaded')
+    #print('cn2 loaded')
     #dense = load_feature('dense', (0, 2048)
-    dense = load_feature('dense', (0, 10, 20, 2048))
-    print('den loaded')
-    print(data.shape)
-    exit()
+    #dense = load_feature('dense', 10**2)
+    #print('den loaded')
+    #print(data.shape)
 
     model = nn_model()
     print(model.summary())
     print('model loaded')
     model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit([x, dense], y, batch_size=batch_size, epochs=epochs, validation_split=0.05)
+    #model.fit([x, dense], y, batch_size=batch_size, epochs=epochs, validation_split=0.05)
+    model.fit_generator(generator = tr_generator,\
+        steps_per_epoch = len(ID_tr)//batch_size,\
+        validation_data = val_generator,\
+        validation_steps = len(ID_val)//batch_size,\
+        epochs=epochs)
+
+    model.save('model.h5')
+    print('model saved')
 
 def load_feature(name, dim):
     data = np.array([]).reshape(dim)
